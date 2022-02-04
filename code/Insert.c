48d330e34ef4b8ef9355619556a33255ff8403ac
Pedro Potenza <pedrohpotenza@gmail.com>
2022-02-04 14:54:06 -0300

    Insert Test

--- code/Insert.c
+++ code/Insert.c
@@ -10,11 +10,11 @@ void insertRegister(REGISTER newRegister) {
     FILE* dataFile = fopen("registers.bin", "r+b");
     fseek(dataFile, 0, SEEK_END);
 
-    printf("Id Cliente: %s\n", newRegister.Id.ClientId);
-    printf("Id Filme: %s\n", newRegister.Id.MovieId);
-    printf("Nome Cliente: %s\n", newRegister.ClientName);
-    printf("Nome Filme: %s\n", newRegister.MovieName);
-    printf("Genero: %s\n", newRegister.Genre);
+    // printf("Id Cliente: %s\n", newRegister.Id.ClientId);
+    // printf("Id Filme: %s\n", newRegister.Id.MovieId);
+    // printf("Nome Cliente: %s\n", newRegister.ClientName);
+    // printf("Nome Filme: %s\n", newRegister.MovieName);
+    // printf("Genero: %s\n", newRegister.Genre);
 
     fwrite(&newRegister.Id.ClientId, 3, sizeof(char), dataFile);
     fwrite(&newRegister.Id.MovieId, 3, sizeof(char), dataFile);
--- insere_busca.cpp
+++ insere_busca.cpp
@@ -12,12 +12,27 @@ int main() {
         char NomeCli[50];
         char NomeF[50];
         char Genero[50];
-    } vet[5] = {{"01", "01", "Joao da Silva", "Filme-1", "Aventura"},              
+    } vet[13] = {{"01", "01", "Joao da Silva", "Filme-1", "Aventura"},              
                 {"01", "02", "Joao da Silva", "Filme-2", "Aventura"},
 				{"01", "03", "Joao da Silva", "Filme-3", "Aventura"},
 				
 				{"02", "03", "Pedro Silva e Silva", "Filme-3", "Aventura"},
-				{"02", "03", "Pedro Silva e Silva", "Filme-3", "Aventura"}};//duplicada
+				{"02", "03", "Pedro Silva e Silva", "Filme-3", "Aventura"},//duplicada
+
+                {"01", "01", "Pedro Silva e Silva", "Filme-5", "Comedia"},//duplicada
+
+				{"02", "01", "Pedro Poteza", "Filme-4", "Comedia"},
+                {"02", "02", "Lets", "Filme-6", "Comedia"},
+
+                {"01", "04", "Nome0104", "Filme-do-0104", "Comedia"},
+                {"01", "05", "Nome0105", "Filme-do-0105", "Comedia"},
+
+                {"01", "06", "Nome0106", "Filme-do-0106", "Comedia"},
+                {"01", "07", "Nome0107", "Filme-do-0107", "Comedia"},
+
+                {"01", "08", "Nome0108", "Filme-do-0108", "Comedia"}
+                
+                };
        
     fd = fopen("insere.bin", "w+b");
     fwrite(vet, sizeof(vet), 1, fd);
@@ -29,7 +44,7 @@ int main() {
         char CodF[3];
     } vet_b[3] = {{"01","01"},
                   {"01","03"},
-				  {"05", "07"}};//não encontrada
+				  {"05", "07"}};//nï¿½o encontrada
     
     fd = fopen("busca.bin", "w+b");
     fwrite(vet_b, sizeof(vet_b), 1, fd);
